5f267cd0beb4d574d4cfa79a351a35d06b489334
infoshailendra020 <infoshailendra020@gmail.com>
2024-04-01 12:55:40 +0530

    fix display settings all pages

---
+++ extensions/meroxio-sticky-mobile-menu/assets/meroxio_sticky_mobile_free.js
@@ -0,0 +1,23 @@
+// Wait for the DOM content to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Find the parent element with the class 'meroxio-sticky-mobile-menu-slider-free'
+    var parentElement = document.querySelector(".m-sticky-menu meroxio-sticky-mobile-menu-pro, .company_name");
+    
+    // Check if the parent element exists
+    if (parentElement) {
+        // Create a new anchor element
+        var poweredByLink = document.createElement("a");
+        poweredByLink.textContent = "Powered By MeroxIO";
+        poweredByLink.href = "https://www.meroxio.com"; // Set the href attribute
+        poweredByLink.style.textDecoration = "none"; // Apply the CSS property
+        poweredByLink.style.display = "block"; // Apply the CSS property
+        poweredByLink.style.setProperty("display", "block", "important");
+        
+        // Append the anchor element to the parent element
+        parentElement.appendChild(poweredByLink);
+
+        // Set display: block; to .company_name
+        parentElement.style.display = "block";
+        parentElement.style.setProperty("display", "block", "important");
+    }
+  });
