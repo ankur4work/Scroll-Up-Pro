07752223d96fd762cf3aeef77201420b5d2cd683
Shivam Merothiya <info@meroxio.com>
2025-02-17 15:58:52 +0530

    fdghjk

--- web/index.js
+++ web/index.js
@@ -47,10 +47,10 @@ app.post(
 
 const PREMIUM_PLAN = 'MeroxIO Premium';
 const MEROXIO = "meroxio";
-const PREMIUM_PLAN_KEY = "floating-cart-button-premium";
+const PREMIUM_PLAN_KEY = "login-with-otp-premium";
 const IS_TEST = true;
-const APP_NAME = "Floating Cart Button";
-const ANALYTICS_DB_PREFIX = "floating_cart_button"
+const APP_NAME = "Login With OTP";
+const ANALYTICS_DB_PREFIX = "login_with_otp"
 const HTTP_STATUS = { OK: 200, BAD_REQUEST: 400, UNAUTHORIZED: 401, INTERNAL_SERVER_ERROR: 500 };
 
 app.use(express.json());
